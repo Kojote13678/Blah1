ee5a3704cd45479cb0140a51bff338b585ed7137
Kojote13678 <33851307+Kojote13678@users.noreply.github.com>
2017-11-21 15:03:07 +1000

    Add files via upload
    These files are for the deletion and re-adding of the wireless profiles

---
+++ WirelessProfileDeletev2.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = "Stop"
+
+$logDir = "C:\Users\jcart285\Documents"
+
+$timeStamp = Get-Date -Format "yyyyMMddHHmm"
+
+$logFile = "PowerShell_transcript_$($timeStamp).txt"
+
+Start-Transcript (Join-Path $logDir $logFile)
+
+function Get-WlanProfiles {
+$wlans = netsh wlan show profiles
+
+$wlans | Select-String '\sIDET'
+$wlans | Select-String '\sQDETA-X'
+}
+
+Stop-Transcript
